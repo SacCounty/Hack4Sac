2e8ae3f6672a28a40b7e1b084607b1e37ada81f7
Christine L. Feaster <feasterc@gmail.com>
2016-03-22 17:08:52 -0700

    Add Listings controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
+	resources :listings do
+    resources :categories, only: [:show]
+  end
 
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
