efaf86d24b169abb5c40ed952d62a64805e3b9be
Koni-Davies <estherkdavies@gmail.com>
2016-03-23 17:35:17 -0700

    Created edit, new, create and update methods for the listings controller.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,15 @@
 class ListingsController < ApplicationController
   def new
+	  @listing = Listing.new
+	  @user = User.all
   end
 
   def create
+	  @listing = Listing.new(params[:listing])
+	  if @listing.save
+		  redirect_to listings_path, :notice => "Your listing has been saved"
+	  else
+	      render "new"
   end
 
   def index
@@ -14,12 +21,19 @@ class ListingsController < ApplicationController
   end
 
   def edit
+	@listing = Listing.find(params[:id])
   end
 
   def update
+    @listing = Listing.find(params[:id])
+	if @listing.update_attributes(params [:listing])
+	    redirect_to listing_path, :notice => "Your listing has been updated"
+	else
+		render "edit"
   end
 
 
   def destroy
   end
 end
+
