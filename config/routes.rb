b97fa4bf6b4cd1597094951ca7b35994f7c07bf0
Joanne <joawu@ucdavis.edu>
2016-03-23 22:10:27 -0700

    add config route

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
     get 'addresses/destroy'
 
   root "home#index"
+	resources :listings do
+    resources :categories, only: [:show]
+  end
 
   devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
