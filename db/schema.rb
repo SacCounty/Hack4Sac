8bda1bdcc1ed141b818e1ac76a05db83c0c5f8be
Christine L. Feaster <feasterc@gmail.com>
2016-03-26 12:42:08 -0700

    Add Type index to users table
     - Typecast user accounts as individual or organization upon creation
     - Index on Type for faster querying of info specific to Organizations and Individuals

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323055517) do
+ActiveRecord::Schema.define(version: 20160326191124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -142,11 +142,13 @@ ActiveRecord::Schema.define(version: 20160323055517) do
     t.string   "name"
     t.string   "entity_name"
     t.string   "entity_license"
+    t.string   "type"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["type"], name: "index_users_on_type", using: :btree
 
   create_table "users_addresses", force: :cascade do |t|
     t.string   "type",       default: "Mailing", null: false
