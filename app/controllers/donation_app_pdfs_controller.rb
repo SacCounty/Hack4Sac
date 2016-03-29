fa92dbf1119e44e293e6072602ca230197ff95de
Erik C. Olson <erikcaineolson@gmail.com>
2016-03-29 16:53:16 -0700

    Separate out controller methods for creating and showing a PDF.

--- app/controllers/donation_app_pdfs_controller.rb
+++ app/controllers/donation_app_pdfs_controller.rb
@@ -1,10 +1,26 @@
 class DonationAppPdfsController < ApplicationController
-  def show
-    user = current_user
-    listing = Listing.find(params[:listing_id])
+  before_action :pdf_params
+  before_action :get_listing
+  before_action :get_user
 
+  def show
     respond_to do |format|
-      format.pdf { send_file DonationApplicationPdf.new(user, listing).export, type: 'application/pdf' }
+      format.html
+      format.pdf { send_file DonationApplicationPdf.new(@user, @listing).export, type: 'application/pdf' }
     end
   end
+
+  protected
+
+    def get_listing
+      @listing = Listing.find(params[:id])
+    end
+
+    def get_user
+      @user = current_user
+    end
+
+    def pdf_params
+      @pdf_params = params.require(:listing).permit(:id)
+    end
 end
