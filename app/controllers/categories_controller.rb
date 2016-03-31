409745b4da3bd391386058bddbad9cd69b4d054f
Christine L. Feaster <feasterc@gmail.com>
2016-03-31 08:55:23 -0700

    Use session to return user to index from show view
     - When User is referred to the Listing#show detailed listing view from listings index, assign previous page to session to preserve any category filters in the index view.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    session.delete(:listings_index)
     category_filters = params[:categories].split(",")
     @categories = Category.all.to_a
     @listings = Listing.joins(:categories).where("name IN (?)", category_filters).to_a
@@ -7,6 +8,7 @@ class CategoriesController < ApplicationController
   end
 
   def index
+    session.delete(:listings_index)
     @categories = Category.all.to_a
     @listings = Listing.all.to_a
     render 'listings/index'
