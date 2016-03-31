c8e850738c7a1beb46fd26adf1c5e479db372ae5
Christine L. Feaster <feasterc@gmail.com>
2016-03-31 08:40:21 -0700

    Authenticate user before interacting with listings
    Guest users may view available donation listings but must be logged in to track a listing or respond to the listing creator regarding items posted.

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,5 +1,5 @@
 module ListingsHelper
   def is_followed?(listing_id)
-    current_user.followed_listings.where(listing_id: listing_id).present?
+    current_user && current_user.followed_listings.where(listing_id: listing_id).present?
   end
 end
