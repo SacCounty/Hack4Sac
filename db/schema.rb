5c9c7f0a18c2f2cbe22a8bf53afe2403d9b17916
Christine L. Feaster <feasterc@gmail.com>
2016-04-01 13:19:15 -0700

    Add submission status to application tracker

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331170553) do
+ActiveRecord::Schema.define(version: 20160401201403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20160331170553) do
     t.text     "approval_status_reason"
     t.datetime "created_at",                                  null: false
     t.datetime "updated_at",                                  null: false
+    t.string   "submission_status"
   end
 
   create_table "followed_listings", force: :cascade do |t|
