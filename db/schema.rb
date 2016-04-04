635a727fbf7ff36c385fe8a412620961aa9a08b0
Christine L. Feaster <feasterc@gmail.com>
2016-04-04 01:28:22 -0700

    Add image url attribute to Listing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160401201403) do
+ActiveRecord::Schema.define(version: 20160401210147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 20160401201403) do
     t.integer  "user_id"
     t.datetime "created_at",                                               null: false
     t.datetime "updated_at",                                               null: false
+    t.string   "image_url"
   end
 
   create_table "listings_categories", force: :cascade do |t|
