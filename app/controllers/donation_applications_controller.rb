e34ddcd827bf01c96b694759876b5e85beb5dca7
Christine L. Feaster <feasterc@gmail.com>
2016-04-05 18:53:39 -0700

    Add Donation Application Controller

--- app/controllers/donation_applications_controller.rb
+++ app/controllers/donation_applications_controller.rb
@@ -1,40 +1,44 @@
 class DonationApplicationsController < ApplicationController
-  before_action :donation_application_params, only: :create
-  before_action :get_listing, only: :create
-  before_action :get_user, only: :create
+  before_action :authenticate_user!
 
   def create
-    @donation_application = DonationApplication.new(@user, @user.questionnaire, @listing)
-    @pdf = DonationApplication.get_pdf_form(@listing, @user)
+    @listing = Listing.find(params[:id])
+    submission_status ||= "printed" if requires_pdf_form?(@listing)
+    submission_status ||= "emailed"
+    da = DonationApplication.new(applicant: current_user, listing: @listing, submission_status: submission_status)
 
-    if @pdf
-      submit_status = "Printed"
+    if donation_application.save
+      @listing.followers << current_user
+      # Mailer send
+      if requires_pdf_form?(@listing)
+        export_pdf and return
+      end
+      flash[:success] = "Your request has been #{submission_status}!"
     else
-      submit_status = "Emailed"
+      flash[:danger] = "Your request was not saved, please try again"
     end
 
-    @donation_application.submit_status = submit_status
-
-    if @donation_application.save
-      @listing.follow
-      flash[:success] = 'Your request has been ' + submit_status + '!'
-    else
-      flash[:danger] = 'Your request was not saved, please try again'
-    end
-
-    redirect_to @listing
+    redirect_to listing_path(@listing)
   end
 
   private
-    def donation_application_params
-      @donation_application_params = params.require(:listing).permit(:id)
-    end
 
-    def get_listing
-      @listing = Listing.find(params[:id])
-    end
+  def export_pdf
+    user_q = current_user.questionnaires.where(name: current_user.account_type).first
 
-    def get_user
-      @user = current_user
+    respond_to do |format|
+      format.pdf(send_file DonationApplicationPdf.new(user_questionnaire: user_q, listing: @listing).export, type: 'application/pdf')
     end
+  end
+
+  def requires_pdf_form?
+    # !Listing.find(listing_id).pdf.nil?
+    @listing.creator.entity_name == "Sacramento County"
+  end
+
+  private
+
+  def donation_application_params
+    params.require(:donation_application).permit(:submission_status, :submission_date, :approval_status, :approval_status_reason)
+  end
 end
