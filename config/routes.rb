534e1cf5586b1ccd985519e0c59078ba6bba6ab9
Erik C. Olson <erikcaineolson@gmail.com>
2016-04-11 03:27:23 -0700

    Allows Eligibility Questions to be Saved. Makes UsersQuestionnaires routes RESTful. Separates 'new' and 'edit' form views. Creates, edits, and saves questionnaire responses. TODO/WIP: When the edit form loads, the radio buttons are *not* pre-selected, and the text boxes contain references to the particular Response instances. I need to sleep before I can finish this...

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,12 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resources :addresses
-    get '/edit-questionnaire', to: 'users_questionnaires#edit'
-    post '/update-questionnaire', to: 'users_questionnaires#update'
+    get '/questionnaires/new', to: 'users_questionnaires#new'
+    get '/questionnaires/edit', to: 'users_questionnaires#edit'
+    post '/questionnaires', to: 'users_questionnaires#create'
+    patch '/questionnaires', to: 'users_questionnaires#create'
+    post '/questionnaires/update', to: 'users_questionnaires#update'
+    patch '/questionnaires/update', to: 'users_questionnaires#update'
     get '/donations', to: 'listings#donation_history'
     get '/requests', to: 'listings#request_history'
     get '/watched_listings', to: 'listings#follow_history'
