84e506360a45264278cfd5d85e08760c632fff62
nealfennimore <hi@neal.codes>
2016-04-11 22:04:23 -0700

    Initialize response instead of creating

--- app/controllers/users_questionnaires_controller.rb
+++ app/controllers/users_questionnaires_controller.rb
@@ -5,7 +5,10 @@ class UsersQuestionnairesController < ApplicationController
 
   def update
     response_params['questions'].each do |question_id, response_hash|
-      question_response = Response.find_or_create_by(user_id: current_user.id, question_id: question_id)
+      question_response = Response.find_or_initialize_by(
+        user_id: current_user.id,
+        question_id: question_id
+      )
       question_response.update(response_text: response_hash['response'])
     end
 
