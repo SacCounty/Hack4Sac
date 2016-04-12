8fd3a50f7bac7502453ebe2026167d54ec96b869
Christine L. Feaster <feasterc@gmail.com>
2016-04-11 18:04:50 -0700

    Allow user to add and edit contact info

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show] do
     resources :addresses
+    resources :contacts, to: 'contact_infos'
     get '/donations', to: 'listings#donation_history'
     get '/requests', to: 'listings#request_history'
     get '/watched_listings', to: 'listings#follow_history'
